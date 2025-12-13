2d4f7601a2704ed21d167297504b8772515a250e
devonnin-a11y <devonnin@gmail.com>
2025-12-13 18:50:05 -0500

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,3 +1,4 @@
+import { state } from "./state.js";
 import { tickNeeds } from "./needs.js";
 import { updateEmotion } from "./emotions.js";
 import { runAutonomy } from "./autonomy.js";
@@ -5,9 +6,22 @@ import { saveGame } from "./storage.js";
 
 export function startClock() {
   setInterval(() => {
+    if (state.time.speed === 0) return;
+
+    state.time.hour += state.time.speed;
+
+    if (state.time.hour >= 24) {
+      state.time.hour = 0;
+      state.time.day += 1;
+    }
+
     tickNeeds();
     updateEmotion();
     runAutonomy();
     saveGame();
   }, 3000);
+}
+
+export function setSpeed(speed) {
+  state.time.speed = speed;
 }
