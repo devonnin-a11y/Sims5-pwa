8f0fd2242084d0cd68c27252262595340616651f
devonnin-a11y <devonnin@gmail.com>
2025-12-13 17:09:34 -0500

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,12 @@
+export const state = {
+  sim: {
+    name: "Sim One",
+    needs: {
+      hunger: 80,
+      energy: 80,
+      social: 80
+    },
+    emotion: "Fine",
+    autonomy: true
+  }
+};
