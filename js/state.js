017247b06bf80e65436bc8722e803facf761b463
devonnin-a11y <devonnin@gmail.com>
2025-12-13 18:46:05 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,6 +1,14 @@
 export const state = {
+  time: {
+    day: 1,
+    hour: 8,
+    speed: 1, // 0=pause,1=normal,2=fast,3=ultra
+  },
+
   sim: {
-    name: "Sim One",
+    name: "New Sim",
+    age: "Young Adult",
+    traits: ["Creative"],
     needs: {
       hunger: 80,
       energy: 80,
