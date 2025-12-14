b5fb3f304697505751809328fa34148b1a703305
devonnin-a11y <devonnin@gmail.com>
2025-12-13 20:06:59 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,21 +3,12 @@ import { startClock } from "./time.js";
 import { renderUI } from "./ui.js";
 import { openCAS } from "./cas.js";
 import { state } from "./state.js";
-import { loadMods } from "./mods.js";
-await loadMods();
 
 loadGame();
 
-// If only default sim exists and still named "New Sim", force CAS create
+// Force CAS on first run
 const active = state.household.sims[state.household.activeSimId];
-const shouldForceCAS = !active || active.name === "New Sim";
-
-if (shouldForceCAS) {
-  openCAS("create");
-}
+if (!active || active.name === "New Sim") openCAS("create");
 
 startClock();
 setInterval(renderUI, 250);
-
-
-
