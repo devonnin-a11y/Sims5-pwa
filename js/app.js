f23107bcd3e851d06b6d23b6a0846f93049fd349
devonnin-a11y <devonnin@gmail.com>
2025-12-13 19:18:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
 import { loadGame } from "./storage.js";
 import { startClock } from "./time.js";
 import { renderUI } from "./ui.js";
-import { openCAS, closeCAS, hydrateCASFromState } from "./cas.js";
+import { openCAS } from "./cas.js";
 import { state } from "./state.js";
-
-// Toggle this:
-// true = skip CAS if a saved sim exists
-// false = always show CAS first
-const AUTO_SKIP_CAS_IF_SAVED = true;
 
 loadGame();
 
-// If saved sim exists, optionally skip CAS
-const hasSavedSim =
-  !!state.sim?.name &&
-  state.sim.name !== "New Sim" &&
-  state.sim.name !== "New Sim";
+// If only default sim exists and still named "New Sim", force CAS create
+const active = state.household.sims[state.household.activeSimId];
+const shouldForceCAS = !active || active.name === "New Sim";
 
-if (AUTO_SKIP_CAS_IF_SAVED && hasSavedSim) {
-  closeCAS();
-} else {
-  openCAS();
-  hydrateCASFromState();
+if (shouldForceCAS) {
+  openCAS("create");
 }
 
 startClock();
