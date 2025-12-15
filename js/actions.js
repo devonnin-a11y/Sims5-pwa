df66bcc7f7b0d50f718edab0e0ee797732ac96ba
devonnin-a11y <devonnin@gmail.com>
2025-12-14 19:17:05 -0500

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -1,15 +1,20 @@
 import { enqueueType } from "./queue.js";
 
-export function eatMeal() { enqueueType("EAT_MEAL"); }
-export function socialize() { enqueueType("SOCIALIZE"); }
-
-export function feelGoodClean() { enqueueType("FEEL_GOOD_CLEAN"); }
-export function feelGoodSelfCare() { enqueueType("FEEL_GOOD_SELFCARE"); }
-export function feelGoodRead() { enqueueType("FEEL_GOOD_READ"); }
-
-export function cookMeal() { enqueueType("COOK_MEAL"); }
-export function nap() { enqueueType("NAP"); }
-
-export function walkToRoom(lotId, roomId) {
-  enqueueType("WALK_TO_ROOM", { lotId, roomId });
+function bind(name, fn) {
+  window[name] = fn;
 }
+
+/* Core actions */
+bind("eatMeal", () => enqueueType("EAT_MEAL"));
+bind("socialize", () => enqueueType("SOCIALIZE"));
+
+bind("feelGoodClean", () => enqueueType("FEEL_GOOD_CLEAN"));
+bind("feelGoodSelfCare", () => enqueueType("FEEL_GOOD_SELFCARE"));
+bind("feelGoodRead", () => enqueueType("FEEL_GOOD_READ"));
+
+bind("cookMeal", () => enqueueType("COOK_MEAL"));
+bind("nap", () => enqueueType("NAP"));
+
+bind("walkToRoom", (lotId, roomId) =>
+  enqueueType("WALK_TO_ROOM", { lotId, roomId })
+);
